2671e23c59f90c38eed778dfffb16c934729e817
NatthapongPop <97676051+NatthapongPop@users.noreply.github.com>
2026-05-29 06:19:29 +0100

    Update Auth Login

--- app/api/controllers/auth_routes.py
+++ app/api/controllers/auth_routes.py
@@ -1,6 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.schemas.user_schema import UserCreate
 from app.core.security import get_password_hash, create_access_token
+
+from app.api.dependencies import get_db
+from app.services.user_service import authenticate_user
+from app.core.security import create_access_token
+from app.schemas.token_schema import Token
 
 router = APIRouter()
 
@@ -15,3 +22,17 @@ async def test_security_funcstion(user_in: UserCreate):
             "hashed_password": hashed_pw,
             "token": token
         }
+
+@router.post("/login", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+    user = await authenticate_user(db, email=form_data.username, password=form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect Email or Password.",
+            headers={"WWW-Authenticfate" : "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": user.email, "role": user.role}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
